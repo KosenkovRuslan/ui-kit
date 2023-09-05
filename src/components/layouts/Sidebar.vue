15219d3e6d243244807738b614a4d14654c5c3b9
Ruslan Kosenkov <kosenkovrus@gmail.com>
2023-09-05 11:54:45 +0300

    Added component Tabs

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -15,6 +15,7 @@ const links = ref([
 	{ name: 'RadioButton', href: '/radiobutton' },
 	{ name: 'Progress', href: '/progress' },
 	{ name: 'Input', href: '/input' },
+	{ name: 'Tabs', href: '/tabs' },
 ])
 </script>
 
---
+++ src/views/Tabs.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import Tabs from '@/components/Tabs.vue';
+
+const names = [
+	{ name: 'About', label: 'О нас'},
+	{ name: 'Vue', label: 'Про vue'},
+	{ name: 'React', label: 'Про React'},
+];
+
+const selectedTab = ref('Vue');
+const changeTab = (tabName) => {
+	selectedTab.value = tabName;
+}
+</script>
+
+<template>
+	<h1 class="heading-1">Tabs</h1>
+	<Tabs
+		:names="names"
+		:selectedTab="selectedTab"
+		@changeTab="changeTab"
+	>
+		<div v-if="selectedTab === 'About'">Lorem ipsum dolor sit amet.</div>
+		<div v-if="selectedTab === 'Vue'">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+		<div v-if="selectedTab === 'React'">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, nostrum.</div>
+	</Tabs>
+</template>
+
+<style lang="scss" scoped></style>
